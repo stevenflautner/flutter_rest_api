3ec55aeb9f3a23baf6334dd7e72070c54779aa4f
StevenFlautner <steven.flautner@gmail.com>
2019-12-29 11:53:22 +0100

    error null check

--- lib/api.dart
+++ lib/api.dart
@@ -4,12 +4,14 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 
 import 'error.dart';
+
+typedef ApiError ErrorDeserializer(Map<String, dynamic> json);
 
 class Api {
 
@@ -18,83 +20,54 @@ class Api {
   final Map<String, String> headers = {
     HttpHeaders.contentTypeHeader: "application/json",
   };
-  final errors = Map<String, ApiError>();
+  final ErrorDeserializer errorDeserializer;
+//  final errors = Map<String, ApiError>();
   final Duration timeout;
 
   Api({
     @required this.ip,
     this.timeout = const Duration(milliseconds: 14000),
-    List<ApiError> errors,
+    this.errorDeserializer
   }) :
         url = 'http://$ip'
   {
-    if (errors != null) {
-      for (ApiError error in errors) {
-        this.errors[error.id] = error;
-      }
-    }
+//    if (errors != null) {
+//      for (ApiError error in errors) {
+//        this.errors[error.id] = error;
+//      }
+//    }
   }
 
   Future post(String path, {@required Object data}) async {
     if (data is! String) data = jsonEncode(data);
 
-    http.Response response;
     try {
-      response = await http.post(url + path, headers: headers, body: data).timeout(timeout);
-    } on SocketException catch (e) {
-      throw ConnectionError();
-    } on TimeoutException catch (e) {
-      throw ConnectionError();
-    }
-
-    if (response.body.isNotEmpty) {
-      final decode = jsonDecode(response.body);
-      print(decode);
-
-      if (response.statusCode == HttpStatus.ok) {
-        return decode;
-      } else {
-        throw errors[decode];
-      }
-
-    } else {
-      if (response.statusCode != HttpStatus.ok) {
-        throw InternalServerError();
-      }
+      return _request(http.post(url + path, headers: headers, body: data));
+    } catch (e) {
+      throw e;
     }
   }
 
   Future<void> delete(String path) async {
-    http.Response response;
     try {
-      response = await http.delete(url + path, headers: headers).timeout(timeout);
-    } on SocketException catch (e) {
-      throw ConnectionError();
-    } on TimeoutException catch (e) {
-      throw ConnectionError();
-    }
-
-    if (response.body.isNotEmpty) {
-      final decode = jsonDecode(response.body);
-      print(decode);
-
-      if (response.statusCode == HttpStatus.ok) {
-        return decode;
-      } else {
-        throw errors[decode];
-      }
-
-    } else {
-      if (response.statusCode != HttpStatus.ok) {
-        throw InternalServerError();
-      }
+      return _request(http.delete(url + path, headers: headers));
+    } catch (e) {
+      throw e;
     }
   }
 
   Future<dynamic> get(String path) async {
+    try {
+      return _request(http.get(url + path, headers: headers));
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<dynamic> _request(Future<Response> future) async {
     http.Response response;
     try {
-      response = await http.get(url + path, headers: headers).timeout(timeout);
+      response = await future.timeout(timeout);
     } on SocketException catch (e) {
       throw ConnectionError();
     } on TimeoutException catch (e) {
@@ -108,7 +81,11 @@ class Api {
       if (response.statusCode == HttpStatus.ok) {
         return decode;
       } else {
-        throw errors[decode];
+        if (errorDeserializer != null) {
+          throw errorDeserializer(decode);
+        } else {
+          throw InternalServerError();
+        }
       }
 
     } else {
--- lib/error.dart
+++ lib/error.dart
@@ -1,15 +1,9 @@
-import 'package:flutter/material.dart';
-
 class ApiError implements Exception {
 
   final String id;
 
   ApiError(this.id);
 
-  String msg(BuildContext context) {
-    return id;
-    //return AppLocalizations.of(context).translate(id) ?? id;
-  }
 }
 
 class InternalServerError extends ApiError { InternalServerError() : super('internal-server-error'); }
